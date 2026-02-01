79dbac0b194809247f2c65158e0adbd0d262844e
AhamedYaseer <ahamedyaseer23@gmail.com>
2026-02-01 23:23:00 +0530

    Optimize Two Sum using hash map approach
    Added an optimized solution using a hash map to improve the two-sum algorithm's efficiency.

--- Arrays/Two Sum.py
+++ Arrays/Two Sum.py
@@ -26,5 +26,31 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
                     if sum==target:
                         return [i,j]
 
+Approach:
+Hash Map
+
+Why this works:
+We are finding a required value (by target - current value)
+If required value is in visited list, return its index
+Else, add current value to visited list
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(n)
 
 
+# --------------------
+# Optimized Solution
+# --------------------
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        visited={}
+        for i in range(n):
+            required_val=target-nums[i]
+            if required_val in visited:
+                return [visited[required_val],i]
+            visited[nums[i]]=i
