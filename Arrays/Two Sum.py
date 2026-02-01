64b39dde7e79d2bc27a379eef1415d757917acb4
AhamedYaseer <ahamedyaseer23@gmail.com>
2026-02-01 23:12:12 +0530

    Add brute force solution for Two Sum
    Implemented brute force solution for Two Sum problem.

--- Arrays/Two Sum.py
+++ Arrays/Two Sum.py
@@ -12,3 +12,19 @@ O(n^2)
 
 Space Complexity:
 O(1)
+
+# --------------------
+# Brute Force Solution
+# --------------------
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        for i in range(0,n):
+            for j in range(1,n):
+                if i!=j:
+                    sum=nums[i]+nums[j]
+                    if sum==target:
+                        return [i,j]
+
+
+
