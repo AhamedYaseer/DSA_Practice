d89c94554708b27843591654be464fd76023a890
AhamedYaseer <ahamedyaseer23@gmail.com>
2026-02-03 00:29:53 +0530

    Implement two-pointer (In-place) method for Remove Element
    Added two-pointer approach to remove elements from the list in place, improving efficiency.

--- Arrays/Remove Element.py
+++ Arrays/Remove Element.py
@@ -27,3 +27,39 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         nums[:]=[i for i in nums if i!=val]
         return len(nums)
+        
+Approach:
+Two pointer (In place)
+
+Why this works:
+
+Maintains two pointers, one for reading (iterates throughout the list) and another for writing (maintains final output)
+writing pointer moves only when a number ❌=val appears
+
+Time Complexity:
+
+O(n)
+
+Space Complexity:
+
+O(1) 
+
+# --------------------
+# two pointer method
+# --------------------
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        write=0
+        read=0
+        n=len(nums)
+        while read<n:
+            if nums[read]!=val:
+                nums[write]=nums[read]
+                write=write+1
+            read=read+1
+        #del nums[write:] here 2,2,_,_, underscore parts are not important
+                        #.use above line if nums should contain only valid nos.
+        return write
+
