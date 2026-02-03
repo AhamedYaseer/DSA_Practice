fcc1124fd01230530b5b5448edfe062bfff79e4d
AhamedYaseer <ahamedyaseer23@gmail.com>
2026-02-04 00:20:06 +0530

    Add math-based solution for Palindrome Number
    Added a detailed explanation of the palindrome problem, approach, and complexities.

--- Strings/Palindrome.py
+++ Strings/Palindrome.py
@@ -27,3 +27,41 @@ class Solution:
         else:
             return False
 
+Problem:
+Palindrome Number
+
+Approach:
+Math (Reverse Half)
+
+Why this works:
+The last half of the number is reversed and compared with the remaining first half.
+For odd-length numbers, the middle digit is ignored. (i.e, 12321 12==12)
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(1)
+
+# ----------------------------------
+# Mathematical reverse (only 2nd half)
+# ----------------------------------
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        if x==0:
+            return True
+        if x%10==0 and x!=0:
+            return False
+
+        rev=0
+        while rev<x: #in the final iteration, rev will be greater than x
+            rev=rev*10+x%10
+            x=x//10
+        
+        if rev==x or rev//10==x: #for 1st condition for even digit (1221: 12 12), 2nd for odd digit (12321: 12 12)
+            return True
+        return False
