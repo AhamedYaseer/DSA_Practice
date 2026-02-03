2778b3324c05051f808a436b5ac28295b01d85b6
AhamedYaseer <ahamedyaseer23@gmail.com>
2026-02-04 00:27:39 +0530

    Implement acronym check in string traversal method
    Added a method to check if a string is an acronym of given words using string traversal.

---
+++ Strings/Check if a String Is an Acronym of Words.py
@@ -0,0 +1,29 @@
+Problem:
+Check if a String Is an Acronym of Words
+
+Approach:
+String Traversal
+
+Why this works:
+Iterates through the list of words, collects the first character of each word and store it in a string(t)
+and compares the string(t) with the given acronym.
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(n)
+
+# -------------------
+# String traversal
+# -------------------
+
+  class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        t=''
+        for i in words:
+            t=t+i[0]
+        if t==s:
+            return True
+        else:
+            return False
